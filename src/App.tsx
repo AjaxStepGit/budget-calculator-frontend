eb2a81f07e0cacc9bba7ce388f93c534221a6622
nalinipathak <nalini.pathak1234@gmail.com>
2025-06-14 10:34:15 +0530

    root url

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import {  Route, Routes } from "react-router-dom"
-import LoginPage from "./pages/login-page"
-import DashboardPage from "./pages/dashboard-page"
-import ProtectedRoute from "./components/private-routes"
-import "./animation.css"
+import { Route, Routes, Navigate } from "react-router-dom";
+import LoginPage from "./pages/login-page";
+import DashboardPage from "./pages/dashboard-page";
+import ProtectedRoute from "./components/private-routes";
+import "./animation.css";
 
+const isAuthenticated = () => {
+  const token = localStorage.getItem("token");
+  const username = localStorage.getItem("username");
+  return !!token && !!username;
+};
 
 function App() {
-
   return (
-   <Routes>
-    <Route path="/login" element={<LoginPage/>} />
-    <Route element={<ProtectedRoute/>}>
-     <Route path="/dashboard" element={<DashboardPage/>} />
-    </Route>
-   </Routes>
-  )
+    <Routes>
+      <Route
+        path="/"
+        element={
+          isAuthenticated() ? (
+            <Navigate to="/dashboard" replace />
+          ) : (
+            <Navigate to="/login" replace />
+          )
+        }
+      />
+      <Route path="/login" element={<LoginPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
